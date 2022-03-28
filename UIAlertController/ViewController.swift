3811a100b7ee0adecb2ac0e917137ee1b75dd2fc
Brendon Zach <brendon_zach@outlook.com>
2022-03-28 14:09:59 -0600

    MVP

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    //MARK:  Declare Variables, Connect Outlets (Public Varaibles)
+    
+    //MARK:  viewDidLoad functions puts items inside the function immediately on the home screen.
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onAddButtonTapped(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "What is your name?", message: nil, preferredStyle: .alert)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        let ok = UIAlertAction(title: "OK", style: .default)
+        {
+            action in
+            
+            print("OK Pressed")
+            
+        }
+        alert.addAction(ok)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(cancel)
+        
+        
+    
+    
+    }
+    
 }
 
